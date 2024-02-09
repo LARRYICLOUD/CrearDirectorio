a2392b8a13f8a49838f518aaace13823ecf46cbe
LARRYSALSA <75591878+LARRYICLOUD@users.noreply.github.com>
2024-02-08 21:30:47 -0500

    Agregar archivos de proyecto.

---
+++ CrearDirectorio/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrearDirectorio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("YO SOY UN PROGRAMADOR Y EXPERTO EN REDES EXCELENTE");
+
+            //Creamos nuestro primer directorio
+            //Directory.CreateDirectory("Mi segunda carpeta\\Mi tercera carpeta");
+            //Console.WriteLine("Creando mi primer carpeta");
+
+            DirectoryInfo directoryInfo = Directory.CreateDirectory("Mi cuarta carpeta");
+
+            //Mostramos informacion del directorio creado, a traves del objeto DirectoryInfo y sus propiedades
+
+            Console.WriteLine($"Nombre: {directoryInfo.Name} ");
+            Console.WriteLine($"Ruta absoluta: {directoryInfo.FullName}");
+            Console.WriteLine($"Fecha de creacion: {directoryInfo.CreationTime}");
+
+            //Console.ReadKey();
+
+        }
+    }
+}
